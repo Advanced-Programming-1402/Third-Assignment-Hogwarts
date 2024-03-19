ab53014e54e3a132ae2f47b9d3776c0325f1a679
Fateme Lashkari <fatemelashkariii84@gamil.com>
2024-03-19 20:52:32 +0330

    some changes happend about menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,42 @@
-/*
-    TODO: Import all the classes that you have defined and make use of them to build the program.
- */
-
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        // TODO: Program starts from here
+        Assistant assistant = new Assistant("" ,"");
+        assistantMenu(assistant);
+        Hogwarts.addAssistant(assistant);
+        runMenu();
+    }
+
+    public static void assistantMenu(Assistant assistant) {
+        System.out.println();
+        Scanner input = new Scanner(System.in);
+        while(true){
+            System.out.println("Hi... Welcome To Our Platform :)  ");
+            System.out.println("Please Enter the number of your desire option :  ");
+            System.out.println("1.Check Profile");
+            System.out.println("2.Teacher Request");
+            System.out.println("3.Remove Student Or Teacher");
+            System.out.println("4.Create A New Course");
+            System.out.println("5.View The Courses");
+            System.out.println("6.Log Out");
+            int choice = input.nextInt();
+            switch(choice){
+                case 1:
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+
+            }
+        }
     }
 
     public static void runMenu() {
